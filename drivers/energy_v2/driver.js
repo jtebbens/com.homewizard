91c851211ea9bd34a8073e3a274089310dc06b35
jeroen@tebbens.net <jeroen@tebbens.net>
2025-02-18 00:12:00 +0100

    v3.4.3

--- drivers/energy_v2/driver.js
+++ drivers/energy_v2/driver.js
@@ -6,101 +6,84 @@ const https = require('https');
 
 module.exports = class HomeWizardEnergyDriverV2 extends Homey.Driver {
 
- 
   async onPair(session) {
 
-    session.setHandler('list_devices', async (data) => {
-      try {
-      const httpsAgent = new https.Agent({
-        rejectUnauthorized: false, //ignore SSL errors
-      });
-  
-      const discoveryStrategy = this.getDiscoveryStrategy();
-      const discoveryResults = discoveryStrategy.getDiscoveryResults();
-      const devices = [];
-      await Promise.all(Object.values(discoveryResults).map(async discoveryResult => {
-        try {
-  
-          // start.html -> Confirmation pressing button on device
-          const payload = {
-            name: 'local/homey_user'
-          };
-          
-          const response  = await fetch(`https://${discoveryResult.address}/api/user`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-Api-Version': '2'
-            },
-            body: JSON.stringify(payload),
-            agent: new (require('https').Agent)({ rejectUnauthorized: false }),
-            
-          });
-  
-          if (!response.ok) {
-            //throw new Error(`Error: ${response.statusText}`);
-            console.log(response.statusText);
-          }
-        } catch( err ) {
-          this.error(discoveryResult.id, err);
-        }
-      }));
-      return devices;
+    session.setHandler("list_devices", async () => {
 
-      }
-      catch (error) {
-        this.homey.app.log(`[Driver] - error`, error);
-        throw new Error(this.homey.__('pair.error'));
-      }
-      })
-      
-      session.setHandler('add_device', async (data) => {
-
-        const httpsAgent = new https.Agent({
-          rejectUnauthorized: false, //ignore SSL errors
-        });
-    
-        const discoveryStrategy = this.getDiscoveryStrategy();
-        const discoveryResults = discoveryStrategy.getDiscoveryResults();
-        const devices = [];
-        await Promise.all(Object.values(discoveryResults).map(async discoveryResult => {
-        try {
-
-        //const result = await response.json();
-        //console.log("Result received: ", result);
-
-        //const responseData = await result.json();
-        const bearer_token = result.token;
-
-        console.log("Bearer token: ", result.token);
+            const httpsAgent = new https.Agent({
+              rejectUnauthorized: false, //ignore SSL errors
+            });
         
-        const res = await fetch(`https://${discoveryResult.address}/api/measurement`, {
-          headers: {
-            'Authorization': `Bearer ${bearer_token}`
-          },
-          agent: new (require('https').Agent)({ rejectUnauthorized: false }) // Ignore SSL errors
-        });
+            const discoveryStrategy = this.getDiscoveryStrategy();
+            const discoveryResults = discoveryStrategy.getDiscoveryResults();
+            const devices = [];
+            await Promise.all(Object.values(discoveryResults).map(async discoveryResult => {
+              try {
+        
+                // start.html -> Confirmation pressing button on device
+                const payload = {
+                  name: 'local/homey_user'
+                };
+                
+                const response  = await fetch(`https://${discoveryResult.address}/api/user`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'X-Api-Version': '2'
+                  },
+                  body: JSON.stringify(payload),
+                  agent: new (require('https').Agent)({ rejectUnauthorized: false }),
+                  
+                });
+        
+                //if (!response.ok) {
+                //  throw new Error(`Error: ${response.statusText}`);
+                //}
+        
+        
+                const result = await response.json();
+                console.log("Result received: ", result);
+        
+                //const responseData = await result.json();
+                const bearer_token = result.token;
+        
+                console.log("Bearer token: ", result.token);
+                
+                const res = await fetch(`https://${discoveryResult.address}/api/measurement`, {
+                  headers: {
+                    'Authorization': `Bearer ${bearer_token}`
+                  },
+                  agent: new (require('https').Agent)({ rejectUnauthorized: false }) // Ignore SSL errors
+                });
+        
+                if( !res.ok )
+                  throw new Error(res.statusText);
+        
+                const data = await res.json();
+        
+                devices.push({
+                  name: data.meter_model,
+                  data: {
+                    id: discoveryResult.id,
+                  },
+                  store: {
+                    token: bearer_token,
+                  }
+                })
+              } catch( err ) {
+                this.error(discoveryResult.id, err);
+              }
+            }));
+            return devices;
+          });
 
-        if( !res.ok )
-          throw new Error(res.statusText);
+          session.setHandler('showView', async (view) => {
+            if (view === 'loading') {
+              
+              await session.nextView();
+            }
+          });
 
-        const data = await res.json();
-
-        devices.push({
-          name: data.meter_model,
-          data: {
-            id: discoveryResult.id,
-          },
-          store: {
-            token: bearer_token,
-          }
-        })
-      } catch( err ) {
-        this.error(discoveryResult.id, err);
-      }
-    }));
-    return devices;
-
-});
 }
+
 }
--- drivers/plugin_battery/driver.js
+++ drivers/plugin_battery/driver.js
@@ -4,96 +4,85 @@ const Homey = require('homey');
 const fetch = require('node-fetch');
 const https = require('https');
 
-module.exports = class HomeWizardPluginBattery extends Homey.Driver {
+module.exports = class HomeWizardEnergyDriverV2 extends Homey.Driver {
 
-  async onPairListDevices() {
+  async onPair(session) {
 
-    async function fetchWithRetry(url, options, maxRetries = 30, delay = 1000) {
-      let attempts = 0;
-  
-      while (attempts < maxRetries) {
-          const response = await fetch(url, options);
-          const result = await response.json();
-  
-          if (response.ok) {
-              return result; // Return the successful response
-          } else if (response.status === 403 && result.error === "user:creation-not-enabled") {
-              attempts++;
-              console.log(`Attempt ${attempts} failed with error: ${result.error}. Retrying in ${delay}ms...`);
-              await new Promise(resolve => setTimeout(resolve, delay));
-          } else {
-              throw new Error(`Error: ${response.statusText}`);
-          }
-      }
-  
-      throw new Error('Max retries reached. Request failed.');
-  }
+    session.setHandler("list_devices", async () => {
 
-    const httpsAgent = new https.Agent({
-      rejectUnauthorized: false, //ignore SSL errors
-    });
-
-    const discoveryStrategy = this.getDiscoveryStrategy();
-    const discoveryResults = discoveryStrategy.getDiscoveryResults();
-    const devices = [];
-    await Promise.all(Object.values(discoveryResults).map(async discoveryResult => {
-      try {
-
-        // start.html -> Confirmation pressing button on device
-        const payload = {
-          name: 'local/homey_user'
-        };
+            const httpsAgent = new https.Agent({
+              rejectUnauthorized: false, //ignore SSL errors
+            });
         
-        const response  = await fetchWithRetry(`https://${discoveryResult.address}/api/user`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Api-Version': '2'
-          },
-          body: JSON.stringify(payload),
-          agent: new (require('https').Agent)({ rejectUnauthorized: false }),
-          
-        });
-
-        if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-        }
-
-
-        const result = await response.json();
-        console.log("Result received: ", result);
-
-        //const responseData = await result.json();
-        const bearer_token = result.token;
-
-        console.log("Bearer token: ", result.token);
+            const discoveryStrategy = this.getDiscoveryStrategy();
+            const discoveryResults = discoveryStrategy.getDiscoveryResults();
+            const devices = [];
+            await Promise.all(Object.values(discoveryResults).map(async discoveryResult => {
+              try {
         
-        const res = await fetch(`https://${discoveryResult.address}/api/measurement`, {
-          headers: {
-            'Authorization': `Bearer ${bearer_token}`
-          },
-          agent: new (require('https').Agent)({ rejectUnauthorized: false }) // Ignore SSL errors
-        });
+                // start.html -> Confirmation pressing button on device
+                const payload = {
+                  name: 'local/homey_user'
+                };
+                
+                const response  = await fetch(`https://${discoveryResult.address}/api/user`, {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'X-Api-Version': '2'
+                  },
+                  body: JSON.stringify(payload),
+                  agent: new (require('https').Agent)({ rejectUnauthorized: false }),
+                  
+                });
+        
+                //if (!response.ok) {
+                //  throw new Error(`Error: ${response.statusText}`);
+                //}
+        
+        
+                const result = await response.json();
+                console.log("Result received: ", result);
+        
+                //const responseData = await result.json();
+                const bearer_token = result.token;
+        
+                console.log("Bearer token: ", result.token);
+                
+                const res = await fetch(`https://${discoveryResult.address}/api/measurement`, {
+                  headers: {
+                    'Authorization': `Bearer ${bearer_token}`
+                  },
+                  agent: new (require('https').Agent)({ rejectUnauthorized: false }) // Ignore SSL errors
+                });
+        
+                if( !res.ok )
+                  throw new Error(res.statusText);
+        
+                const data = await res.json();
+        
+                devices.push({
+                  name: data.meter_model,
+                  data: {
+                    id: discoveryResult.id,
+                  },
+                  store: {
+                    token: bearer_token,
+                  }
+                })
+              } catch( err ) {
+                this.error(discoveryResult.id, err);
+              }
+            }));
+            return devices;
+          });
 
-        if( !res.ok )
-          throw new Error(res.statusText);
-
-        const data = await res.json();
-
-        devices.push({
-          name: data.meter_model,
-          data: {
-            id: discoveryResult.id,
-          },
-          store: {
-            token: bearer_token,
-          }
-        })
-      } catch( err ) {
-        this.error(discoveryResult.id, err);
-      }
-    }));
-    return devices;
+          session.setHandler('showView', async (view) => {
+            if (view === 'loading') {
+              
+              await session.nextView();
+            }
+          });
 
 }
 
