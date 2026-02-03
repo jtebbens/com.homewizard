183fba813c64cd7d718cd991e85df2683b4b2000
jtebbens <jeroen@tebbens.net>
2026-02-03 15:08:22 +0000

    Extra timeout on websocket reconnect with cloud

--- includes/v2/Ws.js
+++ includes/v2/Ws.js
@@ -1,3 +1,13 @@
+/*
+ * HomeWizard WebSocket Manager
+ * Copyright (C) 2025 Jeroen Tebbens
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 'use strict';
 
 const https = require('https');
